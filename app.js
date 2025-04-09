967c36a29753ab1ac0da15ce66fd64eba7649f3f
wellington Pessoa <wellingtonpessoa318@gmail.com>
2025-04-09 20:46:49 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,35 @@
+alert('Boas Vindas ao jogo do número Secreto');
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1 );
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto){
+       chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+
+        if (chute == numeroSecreto) {
+        break; 
+        
+        }
+        else {
+            if (chute > numeroSecreto) {
+                alert(`O número secreto é menor que ${chute}`);
+            } else {
+                alert(`O número secreto é maior que ${chute}`);
+            }
+            tentativas++;
+        }
+     
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+
+if (tentativas == 1){
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+}
+else { 
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+
+}
+
